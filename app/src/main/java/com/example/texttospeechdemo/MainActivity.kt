d00831421f7d5d9499bee26aa0ffc814e4d64810
Sarvadnya2002 <smchiddarwar2002@gmail.com>
2022-08-25 01:26:15 +0530

    Text to speech demo

---
+++ app/src/main/java/com/example/texttospeechdemo/MainActivity.kt
@@ -0,0 +1,63 @@
+package com.example.texttospeechdemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.speech.tts.TextToSpeech
+import android.util.Log
+import android.widget.Toast
+import com.example.texttospeechdemo.databinding.ActivityMainBinding
+import java.util.*
+
+class MainActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
+    private var tts: TextToSpeech? = null
+    private var binding: ActivityMainBinding? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        tts = TextToSpeech(this, this)
+
+        binding?.btnSpeak?.setOnClickListener { view ->
+            if (binding?.etEnteredText?.text!!.isEmpty()) {
+                Toast.makeText(
+                    this,
+                    "Enter a text to speak", Toast.LENGTH_SHORT
+                ).show()
+            } else {
+                speakOut(binding?.etEnteredText?.text.toString())
+            }
+        }
+    }
+
+    override fun onInit(status: Int) {
+        if(status == TextToSpeech.SUCCESS){
+            //todo try different languages
+            val result=tts!!.setLanguage(Locale.US)
+
+            if(result==TextToSpeech.LANG_MISSING_DATA|| result==TextToSpeech.LANG_NOT_SUPPORTED){
+                Log.e("TTS","The language specified is not supported!")
+            }
+        } else {
+            Log.e("TTS","Initialization Failed!")
+        }
+    }
+
+
+    private fun speakOut(text:String){
+        //todo: tru queue flush and queue add
+        tts?.speak(text,TextToSpeech.QUEUE_FLUSH,null,"")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        if(tts!=null){
+            tts?.stop()
+            tts?.shutdown()
+        }
+
+        binding=null
+    }
+}
